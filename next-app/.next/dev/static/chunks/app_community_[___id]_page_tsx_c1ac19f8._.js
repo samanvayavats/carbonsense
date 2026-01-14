9097df2df04823cefb79c748e348072c5d47a5b6
samanvaya <samanvayavats@gmail.com>
2026-01-14 21:47:22 +0530

    pushing again

---
+++ next-app/.next/dev/static/chunks/app_community_[___id]_page_tsx_c1ac19f8._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_20b613ec._.js"
+],
+    source: "dynamic"
+});
